bb0f41377293c747e7a9b251845d6298181d18de
Dan Filipski <dfilipski@firemail.cc>
2023-09-27 14:18:49 -0400

    implement rough do while loop

--- ComputingLab3/ComputingLab3.cpp
+++ ComputingLab3/ComputingLab3.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include "Mass.h"
 
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int quit = 1;
+    Mass m;
+    do
+    {
+        int value;
+        cout << "Please enter 1 to use Avoirdupois pounds, 2 to use Troy pounds, 3 to use grams, or 0 to exit: "
+            cin >> value;
+
+        if (value == 1)
+        {
+            int Avoi;
+            cout << "Please enter a mass in Avoirdupois pounds: ";
+                cin >> Avoi;
+            m.setMassAvoirdupoisPounds(Avoi);
+        }
+        else if (value == 2)
+        {
+            int Troy;
+            cout << "Please enter a mass in Troy pounds: ";
+                cin >> Troy;
+            m.setMassTroyPounds(Troy);
+        }
+        else if (value == 3)
+        {
+            int grams;
+            cout << "Please enter a mass in grams: ";
+                cin >> grams;
+            m.setMassMetricGrams(grams);
+        }
+        else if (value == 0)
+        {
+            quit = 0;
+        }
+
+    } while (quit != 0);
 }
